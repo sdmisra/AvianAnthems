16d34525a20c2b7de9428fce7ef8eb192c5381a5
Joshua Martin <ursa8minority@gmail.com>
2023-05-28 12:39:09 -0600

    Feat cypress testing main page

--- cypress/e2e/MainPage.cy.ts
+++ cypress/e2e/MainPage.cy.ts
@@ -1,5 +1,14 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+
+describe('Main Page Spec', () => {
+  it('should display the main page', () => {
+    cy.visit('http://localhost:3000/'); 
+    cy.get('.header').should('be.visible');
+    cy.get('.info-container').should('be.visible');
+    cy.get('footer').scrollIntoView();
+    cy.get('.about-us').should('be.visible');
+    cy.get(':nth-child(3) > h4').contains('Carol Bradsen')
+    cy.get('[href="https://github.com/cadechaney"]').should('have.attr', 'href' )
+    cy.get('[href="https://www.linkedin.com/in/shanemisra/"]').contains('Linkedin')
+
+  });
+});
