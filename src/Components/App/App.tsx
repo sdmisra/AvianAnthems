c2648359b0dcbaece52dc0ec6aa86875c20e5dbf
Cade Chaney <cadechaney@live.com>
2023-05-30 23:00:52 -0500

    Add first conditional to call the fetch

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -4,13 +4,24 @@ import Header from '../Header/Header'
 import SearchResults from '../SearchResults/SearchResults'
 import { Route, Switch } from 'react-router-dom'
 import mockData from '../../MockData/mock-data.json'
-import { data } from 'cypress/types/jquery'
+// import { data } from 'cypress/types/jquery'
+import callData from '../Api/ApiCalls'
+import fetchRecordings from '../Api/ApiCalls'
+
 
 type AppState = {
   count: number
   currentCnt: string
   currentType: string
   searchResults: Array<{}>
+}
+
+type ApiResponse = {
+  recordings: Recording[]
+}
+
+type Recording = {
+  type: string
 }
 
 class App extends Component<{}, AppState> {
@@ -23,12 +34,26 @@ class App extends Component<{}, AppState> {
 
 fetchResults = (event: React.MouseEvent<HTMLButtonElement>, formState: {selectedCnt : string, selectedType : string}) => {
   event.preventDefault()
-  const filteredData = mockData.recordings.filter(data => 
-       data.cnt === formState.selectedCnt && data.type.includes(formState.selectedType)) 
-    console.log(filteredData)
-  this.setState({searchResults: filteredData})  
-    console.log(this.state)
- 
+  // const filteredData = mockData.recordings.filter(data => 
+  //      data.cnt === formState.selectedCnt && data.type.includes(formState.selectedType)) 
+  //   console.log(filteredData)
+  // this.setState({searchResults: filteredData})  
+    console.log(formState)
+  const combinedParameters = `${formState.selectedCnt}-${formState.selectedType}`
+  switch (combinedParameters) {
+    case 'United States-flight-song':
+      fetchRecordings('https://xeno-canto.org/api/2/recordings?query=cnt:%22=United%20States')
+      .then((data: ApiResponse) => {
+        const filtered = data.recordings.filter(recording => recording.type === 'flight call')
+        console.log(filtered)
+      })
+      
+      break;
+      default:
+        console.log('still works')
+      }
+      
+
   }
 
 render() {
