08633dfbaaed70a1b8fad34baa197df9327c0384
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-06-02 21:18:34 -0600

    fix: Clean up details for props, and bird detail display

--- src/Components/BirdInfo/BirdInfo.tsx
+++ src/Components/BirdInfo/BirdInfo.tsx
@@ -25,22 +25,18 @@ type birdObject = {
 const BirdInfo = ({ chosenBird }:
 BirdProps ) => {
 
+
   console.log("Chosen", chosenBird)
     return ( 
       <div className="info-container">
         <img className="bird" src={assets} alt="two birds looking at you from a branch"></img>
         <section className="bird-data-container">
-          <h2>{chosenBird.en}</h2>
-          <p></p>
-          <p>{chosenBird.stage}</p>
-          <p>{chosenBird.sex}</p>
+          <h3>{chosenBird.en}</h3>
+          <p>{chosenBird.stage} {chosenBird.sex}</p>
           <p>{chosenBird.type}</p>
-            <p>In the background listen for: {chosenBird.also}</p>
-          <p>Recorded in {chosenBird.cnt} at {chosenBird.loc}</p>
-          <p>on {chosenBird.date}</p>
+          <p>Recorded in {chosenBird.cnt} at {chosenBird.loc} on {chosenBird.date}</p>
           <p>Recorded by {chosenBird.rec}</p>
-          <p>Notes from the recorder: {chosenBird.rmk}</p>
-        
+          <p style={{fontStyle: "italic"}}>{chosenBird.rmk}</p>
         </section>
         <section className="audio-details-container">
           <audio src={chosenBird.file} controls className="bird-info-audio"></audio>
