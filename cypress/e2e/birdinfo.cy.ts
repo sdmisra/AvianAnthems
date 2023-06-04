ab29b81fb0ba2e4e011404496aff08043577b8a0
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-06-04 09:57:53 -0600

    feat: Save recent changes before restarting to make cypress run better

--- cypress/e2e/birdinfo.cy.ts
+++ cypress/e2e/birdinfo.cy.ts
@@ -16,11 +16,11 @@ describe('Search Results', () => {
   });
 
   it('should show more information about the chosen bird, Eastern Bluebird', () => {
-    cy.get('.bird-data-container')
+    cy.get('h3')
     .contains('Eastern Bluebird')
-    .contains('male')
-    .contains("flight call")
-    .contains("adult")
+    cy.get('p').contains('male')
+    cy.get('p').contains("flight call")
+    cy.get('p').contains("adult")
     .contains("United States")
     .contains("Avon Park Air Force Range, Avon Park, Polk County, Florida")
     .contains("Patrick J. Blake")
