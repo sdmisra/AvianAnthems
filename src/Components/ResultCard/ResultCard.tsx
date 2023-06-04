244a2e552d032564cd88bff99c4c3581a764dc70
Cade Chaney <cadechaney@live.com>
2023-06-04 13:34:55 -0500

    Add final touches to all components from feedback from team

--- src/Components/ResultCard/ResultCard.tsx
+++ src/Components/ResultCard/ResultCard.tsx
@@ -15,12 +15,14 @@ type ResultCardProps = {
 const ResultCard = ({ bird, stage, sex, songType, performance, country, id, getInfo }: ResultCardProps) => {
   return (
    <div className="result-card">
+    <div className='details'>
       <h4 className="card-header">
         {bird}
       </h4>
       <h5 className="card-detail">
         Location: {country}</h5>
       <h5 className="card-detail">Type: {songType}</h5>
+    </div>
       <audio src={performance} controls className="card-controls"></audio>
       <Link to= {`/info/${id}`}>
         <button className='more-info' id={id} onClick={(event) => getInfo((event.target as HTMLButtonElement ).id)}>Learn More</button>
